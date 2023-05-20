d4024c289561e137f4595da8f211f90f403071fb
William <wdnconsultancy@yahoo.com>
2023-05-20 18:10:50 +0800

    Built backend using flask framework, utlised jinja for formatting

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, redirect, render_template, request
+from flask_session import Session
+
+# Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/deeplearning")
+def deeplearning():
+    return render_template("deeplearning.html")
+
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+
+@app.route("/motivation")
+def motivation():
+    return render_template("motivation.html")
+
+
+
