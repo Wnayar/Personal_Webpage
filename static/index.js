c634449a64e2aefef08e15aee6500b63a83eab91
William <wdnconsultancy@yahoo.com>
2023-05-23 14:25:46 +0800

    Fixed home button bugs which required 2 clicks instead of one

--- static/index.js
+++ static/index.js
@@ -1,14 +1,8 @@
 function redirectinsight(){
-   button1home = document.querySelector("#button1home");
-   button1home.addEventListener("click", function (){
     location.href = "/insights";
-   });
 }
 
 
 function redirectmotivation(){
-    button1home = document.querySelector("#button2home");
-    button1home.addEventListener("click", function (){
-     location.href = "/motivation";
-    });
+     location.href = "/motivation" 
  }
