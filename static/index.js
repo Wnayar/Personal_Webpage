3839966e00a2e2a9bf35365ede7c5004fd260af1
William <wdnconsultancy@yahoo.com>
2023-05-23 13:41:15 +0800

    Added snippets on homepage to other pages, moved gyroscope to bottom: Local Version Done

---
+++ static/index.js
@@ -0,0 +1,14 @@
+function redirectinsight(){
+   button1home = document.querySelector("#button1home");
+   button1home.addEventListener("click", function (){
+    location.href = "/insights";
+   });
+}
+
+
+function redirectmotivation(){
+    button1home = document.querySelector("#button2home");
+    button1home.addEventListener("click", function (){
+     location.href = "/motivation";
+    });
+ }
